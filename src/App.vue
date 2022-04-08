167ca27b3e52bf2ebf1c7afc54bac704c402cb2a
Arko Roychowdhury <77418610+arkorch@users.noreply.github.com>
2022-04-08 07:39:38 -0400

    set up

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="app-wrapper">
+    <header class="col-sm-12 roku-header">
+      <img src="@/assets/images/netflix.svg" alt="Roku logo" width="150"><span>Dynamic Clone</span>
+      <!-- source @/assets/images/roku.svg-->
+      <nav class="float-right">
+      <!-- <ul>            
+        <li><i class="fas fa-user-circle"></i></li>
+        <li><i class="fas fa-cog"></i></li>
+        <li><i class="fas fa-power-off"></i></li>					
+      </ul> -->
+
+      </nav>
+    </header>
+    <router-view></router-view>
+  </section>
+</template>
+
+<style lang="scss">
+  @import "@/assets/sass/vars.scss";
+  @import "@/assets/sass/main.scss";
+</style>
---
+++ src/views/UserSelectVue.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="users">
+        <h1>Who's using Roku?</h1>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: 'TheUsersView',
+
+    created() {
+        fetch('/users/getall')
+            .then(res => res.json())
+            .then(data => console.table(data))
+        .catch(err => console.error(err));
+    }
+}
+</script>
